959435f610b02da5631e7860cab1e2706f69ad59
Ricardo Fiorani <1641075+ricardofiorani@users.noreply.github.com>
2025-07-29 21:48:20 +0200

    chore: add line stating bodyguard script is experimental

--- BodyGuard.ts
+++ BodyGuard.ts
@@ -1,10 +1,13 @@
 /**
+ * EXPERIMENTAL: This script is experimental and may not work as expected.
+ *
  * Bodyguard script
  * This script allows the player to spawn a bodyguard that follows them around.
  * Press Key.K to spawn a bodyguard.
  * The bodyguard will follow the player and enter their vehicle if they are in one.
  * If the player is not in a vehicle, the bodyguard will walk alongside them.
  * If the bodyguard is dead, it will be removed and a new one can be spawned.
+ *
  */
 
 import {Key} from ".config/enums";
